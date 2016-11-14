b5243d1d2f75e01e870746da8a3a1e5c3a710d0b
Travis Smith <flagezooke@gmail.com>
2016-11-14 12:37:00 -0700

    Item objects can be built from the static data

---
+++ Item.java
@@ -0,0 +1,65 @@
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import com.google.gson.Gson;
+
+class Item{
+    
+    
+    public String id, name, description;
+    public String colloq, plaintext;
+    public List<String> into, from, tags;
+    public int cost_total, cost_combine, cost_sell;
+    
+    public String sprite;
+    public int x,y,w,h; // location within the sprite
+    
+    public Item(int itemId){
+        this(String.format("%04d",itemId)); // call Item's other constructor
+    }
+    
+    public Item(String itemId){
+        Map<String, Object> json = convertItemIdToMap(itemId); // call Item's other constructor
+        setConstants(json);
+    }
+    
+    public Item(Map<String, Object> json){
+        setConstants(json);
+    }
+    
+    private Map<String, Object> convertItemIdToMap(String itemId){
+        id = itemId;
+        // find the region being used
+        FileManager fileManager = new FileManager();
+        Map<String, String> settings = fileManager.readSettings();
+        String regionSlug = settings.get("region");
+        
+        // Read the item.json file from the region's folder
+        String itemJson = new String(fileManager.loadFile(true, "regions/" + regionSlug, "item.json"));
+        Gson gson = new Gson();
+        Map<String, Object> items = gson.fromJson(itemJson, Map.class);
+        
+        Map<String, Object> data = (Map<String, Object>)items.get("data");
+        return (Map<String, Object>)data.get(itemId);
+    }
+
+    private void setConstants(Map<String, Object> json){
+        name = (String)json.get("name");
+        description = (String)json.get("description");
+        colloq = (String)json.get("colloq");
+        plaintext = (String)json.get("plaintext");
+        
+        into = (List)json.get("into");
+        from = (List)json.get("from");
+        tags = (List)json.get("tags");
+        
+        Map<String, Object> image = (Map<String, Object>)json.get("image");
+        sprite = (String)image.get("sprite");
+        x = ((Double)image.get("x")).intValue();
+        y = ((Double)image.get("y")).intValue();
+        h = ((Double)image.get("h")).intValue();
+        w = ((Double)image.get("w")).intValue();
+    }
+
+}
